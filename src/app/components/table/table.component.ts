ae23c6d6d991eea4b57cba3bf8adee1ca208e714
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-04-16 19:09:30 +0200

    feat(Table): display data

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
+import { DataRow, TableStore } from "./table.store";
 
 @Component({
   selector: "app-table",
   templateUrl: "./table.component.html",
   styleUrls: ["./table.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [TableStore],
 })
 export class TableComponent implements OnInit {
-  constructor() {}
+  @Input() set dataRows(value: DataRow[] | null) {
+    // TODO: get rid of null
+    this.tableStore.setDataRows(value || []);
+  }
+
+  vm$ = this.tableStore.vm$;
+
+  constructor(private tableStore: TableStore) {}
 
   ngOnInit(): void {}
 }

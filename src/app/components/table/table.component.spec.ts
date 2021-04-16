64974d26c0995f0af7bd24e22c9810f3b7a04c3e
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-04-16 10:28:24 +0200

    chore: ng add @schuchard/prettier

--- src/app/components/table/table.component.spec.ts
+++ src/app/components/table/table.component.spec.ts
@@ -1,16 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { TableComponent } from './table.component';
+import { TableComponent } from "./table.component";
 
-describe('TableComponent', () => {
+describe("TableComponent", () => {
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TableComponent ]
-    })
-    .compileComponents();
+      declarations: [TableComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('TableComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });

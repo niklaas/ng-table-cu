342e57f00dc82727e3adee4e76e2da1303d9b834
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-04-16 11:07:01 +0200

    feat(PostsService): implement getAll() posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { TableModule } from "./components/table/table.module";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TableModule],
+  imports: [BrowserModule, HttpClientModule, TableModule],
   providers: [],
   bootstrap: [AppComponent],
 })

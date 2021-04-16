2b3bfeddcf7f95207d6d81978e6c2ce69792b037
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-04-16 10:23:23 +0200

    feat(App): add TableComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {TableModule} from './components/table/table.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/table/table.module.ts
+++ src/app/components/table/table.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table.component';
 
-
-
 @NgModule({
   declarations: [
+    TableComponent
+  ],
+  exports: [
     TableComponent
   ],
   imports: [

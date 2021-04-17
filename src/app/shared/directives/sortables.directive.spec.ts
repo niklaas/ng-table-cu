a81e57dc0b755e812473628799dc7add6d7ebe74
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-04-17 22:54:00 +0200

    chore(Sortables): add

---
+++ src/app/shared/directives/sortables.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SortablesDirective } from './sortables.directive';
+
+describe('SortablesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SortablesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SortablesDirective } from './directives/sortables.directive';
+import { SortableDirective } from './directives/sortable.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SortablesDirective,
+    SortableDirective
+  ],
   imports: [
     CommonModule
   ]

a0232ede757e39ddb2ed35bf9fe21156bc460b9a
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-04-17 22:50:18 +0200

    chore(Shared): add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppComponent } from "./app.component";
 import { TableModule } from "./components/table/table.module";
-import { HttpClientModule } from "@angular/common/http";
+
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
 
 import { PostsEffects } from "./store/effects/posts.effects";
 import { postsReducer } from "./store/reducers/posts.reducer";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
